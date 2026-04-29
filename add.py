1795e80e29a46ddabcc6165de79bd85e43b5fcca
PadmashreeBA <padmashreeba088@gmail.com>
2026-04-29 19:47:37 +0530

    Update add.py

--- add.py
+++ add.py
@@ -1,11 +1,9 @@
-# Python Addition Program
 
-# Step 1: Take input from the user
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
-# Step 2: Perform addition
+
 sum_result = num1 + num2
 
-# Step 3: Display the result
+
 print("The sum is:", sum_result)
